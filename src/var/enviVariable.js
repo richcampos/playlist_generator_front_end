14f9b50871b87b314a5c5a094b6d203c77fa55a5
Ricardo Campos Martinez <ricamposm@MacBook-Air-de-Ricardo.local>
2020-07-02 17:54:22 -0500

    redirect uri

--- src/var/enviVariable.js
+++ src/var/enviVariable.js
@@ -1,6 +1,6 @@
 export const CLIENTID = 'ab9b8f77957f4befbd978a5e3c9d5365'
 export const ACCESSURL = 'https://accounts.spotify.com/authorize'
-export const REDIRECTURI = 'http://localhost:8080/callback'
-// export const REDIRECTURI = 'https://bargi.netlify.app/callback'
+// export const REDIRECTURI = 'http://localhost:8080/callback'
+export const REDIRECTURI = 'https://bargi.netlify.app/callback'
 // export const REDIRECTURI = 'https://bargi.herokuapp.com/'
 export const SCOPE = 'playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private%20user-library-read%20user-library-modify%20user-read-private%20user-top-read'
